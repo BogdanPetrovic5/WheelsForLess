9f6c0d3ecc396a216aecc2cb3aaf328027bd4d2e
Bogdan <petrovic215@gmail.com>
2023-09-29 21:15:03 +0200

    Login-Register component created. Initial design added.

--- Frontend/UsedCarsShopFrontend/src/app/app.module.ts
+++ Frontend/UsedCarsShopFrontend/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginRegisterComponent } from './login-register/login-register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginRegisterComponent
   ],
   imports: [
     BrowserModule,
